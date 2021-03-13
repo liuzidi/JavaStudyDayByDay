0907875aa1ae2909af4582227350e1dc5c197f3d
liuzidi <liuzidi17@163.com>
2021-03-13 15:59:49 +0800

    comit test2

--- JavaCode/day05/src/OverRideTest/OverRideTest.java
+++ JavaCode/day05/src/OverRideTest/OverRideTest.java
@@ -2,7 +2,9 @@ package OverRideTest;
 
 public class OverRideTest {
     public static void main(String[]args){
-
+    Students s1 = new Students();
+    s1.showSuperPrivate();
+    s1.showSuperNo();
     }
 }
 //子类重写的权限修饰符不小于父类被重写的方法的权限修饰符
@@ -25,5 +27,32 @@ class Person{
     public String getName() {
         return name;
     }
+    private void superPrivate(){
+        System.out.println("这是父类Person的私有方法");
+    }
+    void superNO(){
+        System.out.println("这是父类Person的缺省方法");
+    }
+    protected void superProtected(){
+        System.out.println("这是父类Person的保护方法");
+    }
 
+    public void superPublic(){
+        System.out.println("这是父类Person的公共方法");
+    }
+}
+class Students extends Person{
+    //重写方法
+    private void superPrivate(){
+        System.out.println("这是子类Students的重写私有方法");
+    }
+    public void showSuperPrivate(){
+        superPrivate();
+    }
+    private void superNo(){
+        System.out.println("这是子类Students的重写缺省方法");
+    }
+    public void showSuperNo(){
+        superNo();
+    }
 }
