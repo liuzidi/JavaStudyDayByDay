af74a942f3fa25a9d7ef8ba5dff7dd7b07af0096
liuzidi <liuzidi17@163.com>
2021-03-24 11:38:11 +0800

    commit

--- JavaCode/day16/src/Test/Test.java
+++ JavaCode/day16/src/Test/Test.java
@@ -1,5 +1,14 @@
 package Test;
 
 public class Test {
-
+    public void method(){
+        int num =2;
+        class AA{
+            public void show(){
+                //num =5;//Variable 'num' is accessed from within inner class,
+                // needs to be final or effectively final
+                System.out.println(num);
+            }
+        }
+    }
 }
