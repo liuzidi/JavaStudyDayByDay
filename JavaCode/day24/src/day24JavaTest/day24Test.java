efe8890da986addbba307a39b8f7b34a76f912cf
liuzidi <liuzidi17@163.com>
2021-04-05 13:45:15 +0800

    commit

--- JavaCode/day24/src/day24JavaTest/day24Test.java
+++ JavaCode/day24/src/day24JavaTest/day24Test.java
@@ -1,9 +1,72 @@
 package day24JavaTest;
 
+import org.junit.Test;
+
+
+import java.io.*;
+
 /**
  * @author:liuzidi
  * @Description:
+ * 自定义对象IO测试：需要满足要求才能序列化
  */
 public class day24Test {
+    @Test
+    public  void test1() {
+        ObjectOutputStream oos =null;
+        try {
+            oos =new ObjectOutputStream(new FileOutputStream("ok.dat"));
+            oos.writeObject(new Person(28,"小明"));
+            oos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(oos != null){
+                try {
+                    oos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
+
+    @Test
+    public  void test2() {
+        ObjectInputStream ois =null;
+        try {
+            ois =new ObjectInputStream(new FileInputStream("ok.dat"));
+            Object obj =ois.readObject();
+            Person p =(Person)obj;
+            System.out.println(p);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if(ois != null){
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+class Person implements Serializable {
+    public static final long serialVersionUID =55656L;
+    int age;
+    String name;
+    Person(int age, String name){
+        this.age=age;
+        this.name =name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
